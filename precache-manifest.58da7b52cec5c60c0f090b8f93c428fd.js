42a6bc34e905aa88dd6919a92426ab4f1b4277a5
prstwo <prs2.pari@gmail.com>
2020-09-05 00:30:26 +0430

    Updates

---
+++ precache-manifest.58da7b52cec5c60c0f090b8f93c428fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83db621e06bbbda91b986b222c17b7ac",
+    "url": "/parastoo-alr-resume/index.html"
+  },
+  {
+    "revision": "03124ece22af6cbf38f3",
+    "url": "/parastoo-alr-resume/static/css/main.a0605cda.chunk.css"
+  },
+  {
+    "revision": "bc508cf8d837f12b0605",
+    "url": "/parastoo-alr-resume/static/js/2.56c58e96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/parastoo-alr-resume/static/js/2.56c58e96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03124ece22af6cbf38f3",
+    "url": "/parastoo-alr-resume/static/js/main.e9ac5eae.chunk.js"
+  },
+  {
+    "revision": "8a48a34bea8462ea6223",
+    "url": "/parastoo-alr-resume/static/js/runtime-main.d8eac3ae.js"
+  }
+]);
